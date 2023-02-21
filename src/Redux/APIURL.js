73ea8c7c1e934847e3ab9031179e98f75b1366ca
bikash-barik <bikashbarik16623@gmail.com>
2023-02-21 21:35:41 +0530

    Comit For new urls build s

--- src/Redux/APIURL.js
+++ src/Redux/APIURL.js
@@ -1,2 +1,2 @@
-export const APIURL = "https://api.websyetem.online";
-// export const APIURL = "http://13.59.53.52:5000";
+// export const APIURL = "https://api.websyetem.online";
+export const APIURL = "http://13.59.53.52:5000";
